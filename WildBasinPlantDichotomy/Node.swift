562f0d21458630272a0e88992cea43940ebf2d48
Gage Martin <cluggelite@gmail.com>
2017-05-21 16:24:26 -0500

    Made changes to Node.swift

--- WildBasinPlantDichotomy/Node.swift
+++ WildBasinPlantDichotomy/Node.swift
@@ -24,16 +24,42 @@ class Node
     }
 }
 
+//Here we convert Node to the value
+extension Node: CustomStringConvertible {
+    // 2
+    var description: String {
+        // 3
+        let text = "\(value)"
+        
+        // 4
+        //if !children.isEmpty {
+        //    text += " {" + children.map { $0.description }.joined(separator: ", ") + "} "
+        //}
+        return text
+    }
+}
+//Start of root (level 0)
 let plants = Node(value: "Plants")
 
+//First 2 nodes (level 1)
+let simple = Node(value: "Are the leaves SIMPLE")
+let compound = Node(value: "or COMPOUND?")
 let palmate = Node(value: "Are the leaves PALMATE,")
 let palmateRedBuckeye = Node(value: "Red Buckeye")
 let palmateLavenderChasteTree = Node(value: "Lavender Chaste-Tree")
 let palmateVirginiaCreeper = Node(value: "Virginia Creeper")
 let palmateSevenLeafCreeper = Node(value: "Seven-Leaf Creeper")
 
+//Values of SIMPLE child
+let alternate = Node(value: "Are the leaves ALTERNATE")
+let opposite = Node(value: "or OPPOSITE?")
 let pinnate = Node(value: "PINNATE,")
 
+//Values of ALTERNATE child
+let alternateVine = Node(value: "Is it a VINE")
+let alternateNotAVine = Node(value: "or NOT A VINE?")
+
+//Values of THREE LEAFLETS (Leaves)
 let threeLeaflets = Node(value: "Does the plant have THREE,")
 let threeLeafletsCowItchVine = Node(value: "Cow-Itch Vine")
 let threeLeafletsPoisonIvy = Node(value: "Poison Ivy")
@@ -42,6 +68,15 @@ let threeLeafletsFragrantSumac = Node(value: "Fragrant Sumac")
 let threeLeafletsSouthernDewberry = Node(value: "Southern Dewberry")
 let threeLeafletsAgarita = Node(value: "Agarita")
 
+//Values of VINE child (leaf)
+let vineGreenbriar = Node(value: "Greenbriar")
+let vineMustangGrape = Node(value: "Mustang Grape")
+let vineRattanVine = Node(value: "Rattan Vine")
+let vineCowItchVine = Node(value: "Cow Itch Vine")
+let vineWinterGrape = Node(value: "Winter Grape")
+let vineCarolinaSnailseed = Node(value: "Carolina Snailseed")
+
+//Values of Four to Ten Leaflets (Leaves)
 let fourToTenLeaflets = Node(value: "FOUR to TEN,")
 let fourToTenLeafletsSouthernDewberry = Node(value: "Southern Dewberry")
 let fourToTenLeafletsPeppervine = Node(value: "Peppervine")
@@ -51,12 +86,29 @@ let fourToTenLeafletsEvergreenSumac = Node(value: "Evergreen Sumac")
 let fourToTenLeafletsBoxElder = Node(value: "Box Elder")
 let fourToTenLeafletsRedTexasAsh = Node(value: "Red/Texas Ash")
 
+//Values of NOT A VINE child
+let smooth = Node(value: "Are the leaf edges SMOOTH,")
+let scalloped = Node(value: "SCALLOPED,")
+let toothed = Node(value: "TOOTHED,")
+let lobed = Node(value:"or LOBED?")
 let greaterThanTenLeaflets = Node(value: "or MORE THAN TEN leaflets?")
 
+//Values of SMOOTH child (leaf)
 let lessThanHalfInch = Node(value: "Are leaflets LESS than HALF INCH wide,")
 
+//Values of SCALLOPED child (leaf)
 let moreThanHalfInch = Node(value: "Or GREATER than HALF INCH wide?")
 
+//Values of TOOTHED
+
+
+//Values of SMALLER THAN 2" IN LENGTH child (leaf)
+
+//Values of LARGER THAN 2" IN LENGTH child (leaf)
+
+//Values of OPPOSITE child
+let oppositeVine = Node(value: "Is it a VINE")
+let oppositeNotAVine = Node(value: "green")
 let doublePinnate = Node(value: "or DOUBLE PINNATE?")
 let doublePinnateHoneyMesquite = Node(value: "Honey Mesquite")
 let doublePinnateCatsClaw = Node(value: "Cat's Claw")
