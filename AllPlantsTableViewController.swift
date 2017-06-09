1ce9ece3c173a1a975b7e38ae69a49e1c8e1db2e
Phillip Valdez <stevehash@phillips-mbp>
2017-06-08 19:35:40 -0500

    Didn't Fix to much. Added an image. We need to figure out how to change an image based on a name from an array

--- AllPlantsTableViewController.swift
+++ AllPlantsTableViewController.swift
@@ -12,7 +12,9 @@ import UIKit
 class AllPlantsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
     //Creating array of plant names
-    var plantData = ["Greenbriar", "Mustang Grape", "Rattan Vine", "Cow Itch Vine", "Winter Grape", "Carolina Snailseed", "Live Oak", "Gum Elastic", "Texas Persimmon", "Osage Orange", "Chinese Tallow", "Bush Croton", "Cenizo", "Redbud", "Sugar Hackberry", "Buckthorn", "Netleaf Hackberry", "Texas Madrone", "Deciduous Yaupon", "Yaupon", "Mouse-ears", "Snakewood", "Ceanothus", "Payonia", "Cedar Elm", "Netleaf Hackberry", "Black Willow", "False Willow", "American Elm", "Black Cherry", "Texas Mulberry", "Coral Honeysuckle", "White Honeysuckle", "Japanese Honeysuckle", "Button Bush", "Roughleaf Dogwood", "Silktassel", "Privet", "Elbow Bush", "Viburnum", "Shrubby Boneset", "Wand Butterfly Bush", "Texas Lantana", "American Beautyberry", "Red Buckeye", "Lavender Chaste-tree", "Virginia Creeper", "Seven-leaf Creeper", "Cow-itch Vine", "Poison Ivy", "Wafer Ash", "Fragrant Sumac", "Southern Dewberry", "Agarita", "Southern Dewberry", "Peppervine", "Mexican Buckeye", "Moutain Laurel", "Evergreen Sumac", "Box Elder", "Red Ash", "Black Dalea", "Texas Kidneywood", "Retama", "Indigo Bush", "Eve's Necklace", "Lindheimer Senna", "Flameleaf Sumac", "Soapberry", "Walnuts", "Pecans", "Toothache Tree", "Honey Mesquite", "Cat's Claw", "Pink Mimosa", "Chinaberry" ]
+   // var plantData = ["Greenbriar", "Mustang Grape", "Rattan Vine", "Cow Itch Vine", "Winter Grape", "Carolina Snailseed", "Live Oak", "Gum Elastic", "Texas Persimmon", "Osage Orange", "Chinese Tallow", "Bush Croton", "Cenizo", "Redbud", "Sugar Hackberry", "Buckthorn", "Netleaf Hackberry", "Texas Madrone", "Deciduous Yaupon", "Yaupon", "Mouse-ears", "Snakewood", "Ceanothus", "Payonia", "Cedar Elm", "Netleaf Hackberry", "Black Willow", "False Willow", "American Elm", "Black Cherry", "Texas Mulberry", "Coral Honeysuckle", "White Honeysuckle", "Japanese Honeysuckle", "Button Bush", "Roughleaf Dogwood", "Silktassel", "Privet", "Elbow Bush", "Viburnum", "Shrubby Boneset", "Wand Butterfly Bush", "Texas Lantana", "American Beautyberry", "Red Buckeye", "Lavender Chaste-tree", "Virginia Creeper", "Seven-leaf Creeper", "Cow-itch Vine", "Poison Ivy", "Wafer Ash", "Fragrant Sumac", "Southern Dewberry", "Agarita", "Southern Dewberry", "Peppervine", "Mexican Buckeye", "Moutain Laurel", "Evergreen Sumac", "Box Elder", "Red Ash", "Black Dalea", "Texas Kidneywood", "Retama", "Indigo Bush", "Eve's Necklace", "Lindheimer Senna", "Flameleaf Sumac", "Soapberry", "Walnuts", "Pecans", "Toothache Tree", "Honey Mesquite", "Cat's Claw", "Pink Mimosa", "Chinaberry" ]
+    
+    var plantData = ["Pink Mimosa"]
     
     //Adding tableview
     @IBOutlet weak var tableView: UITableView!
@@ -22,6 +24,8 @@ class AllPlantsTableViewController: UIViewController, UITableViewDelegate, UITab
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+    
     }
     
     //Returns the number of sections in table
@@ -44,6 +48,7 @@ class AllPlantsTableViewController: UIViewController, UITableViewDelegate, UITab
         
         cell.textLabel?.text = plantData[indexPath.row];
         
+    
         return cell
     }
 
