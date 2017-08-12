c3bc67b8123d541f54c6f7a659d15eac9429fe11
Phillip Valdez <stevehash@x-10-5-56-196.wifi.stedwards.edu>
2017-08-12 18:53:44 -0500

    Fixed the back button function

--- WildBasinPlantDichotomy/FinalDetailViewController.swift
+++ WildBasinPlantDichotomy/FinalDetailViewController.swift
@@ -20,23 +20,29 @@ class FinalDetailViewController: UIViewController
     @IBOutlet weak var plantImageView: UIImageView!
     @IBOutlet weak var plantDescriptionLabel: UILabel!
     @IBOutlet weak var plantURLLabel: UILabel!
-    @IBOutlet weak var plantName: UINavigationItem!
+
     
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+        let newBackButton = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.plain, target: self, action: #selector(FinalDetailViewController.back(sender:)))
+        self.navigationItem.leftBarButtonItem = newBackButton
+        
         setUI()
         
         
     }
     
-    @IBAction func navButtonBack(_ sender: Any)
-    {
+    
+    func back(sender: UIBarButtonItem) {
         let myVC = storyboard?.instantiateViewController(withIdentifier: "final") as! FinalPlantViewController
         myVC.plantCode = plantCode
         navigationController?.pushViewController(myVC, animated: true)
+        
     }
+    
     
     func setUI()
     {
@@ -46,9 +52,7 @@ class FinalDetailViewController: UIViewController
         
         
         //plantURLLabel.text = plantURL
-        
-        
-        
+    
         
     }
     
